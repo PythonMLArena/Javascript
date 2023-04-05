00f3b3a323a3141d4e54d1e60be5b563d623c2ff
Prathamesh <prathameshmane855@gmail.com>
2023-04-05 20:54:04 +0530

    Day 1 with Javascript

---
+++ JS Basics/Basics.js
@@ -0,0 +1,22 @@
+// Basics of Javascript
+
+// Special words which have special meaning and cannot use for own purpose called as keywords
+
+// ex. of Keywords let , for , const , if , else , return etc.
+
+// we store values we use Variables.
+// variables are the memory buckets that store the value and has name which can be used to access the bucket 
+// Inside variables we used to store or hold the data
+
+let x = 12; // x is label and 12 is value 
+
+
+// One complete instruction is called statement and in the end of the statement we put the semicolon.
+
+
+// it is recommended practice
+
+ 
+
+
+
---
+++ JS Basics/variables.js
@@ -0,0 +1,34 @@
+// 3 ways to create a variable
+// scopes part will help understand why different ways to declare the variables
+// 1st way
+
+var pratham = 5;
+
+console.log(pratham);
+
+var playername= "Prathamesh";
+
+console.log(playername);
+
+// 2nd way 
+
+let pratham1=6;
+let age = 34;
+age=35; // can change the value of age 
+console.log(pratham1);
+console.log(age);
+
+
+// 3rd way 
+// cannot change the x values after the declaration
+const x= 100;
+
+console.log(x);
+
+// Naming Rules for variable creation 
+
+//1. Variable can contain small alphabets, capital alphabets , digits , underscore (_) .
+//2. we cannot have space or any other special character.
+//3. variable cannot start with a digit but it can have digit inbetween or at last. and cannot use the keywords 
+// let 0james=10; it is wrong
+  
