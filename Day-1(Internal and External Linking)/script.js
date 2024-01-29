81b8386f0d0bee9efa70c0c625f0909fdd4d7df9
Prathamesh <prathameshmane855@gmail.com>
2024-01-29 17:18:12 +0530

    Exection context notes are added

--- Day-1(Internal and External Linking)/script.js
+++ Day-1(Internal and External Linking)/script.js
@@ -10,3 +10,16 @@ console.log("This is Prathamesh")
 // JS is Synchronous and single threaded Language 
 // Synchronous Single threaded = execute one command at a time in specified order 
 
+// There are 2 phases in Gloabl Execution Phase
+// 1. Creation Phase (Memory Creation)
+    // - Variable and function are stored as key value pair 
+    // - Variable value will be undefined 
+    // - Function value will be whole function derivation code
+
+// 2. Code execution phase
+    // - JS runs code 1 by 1 line
+    // - Variable values are filled 
+    // - Function invokation means new execution context is created with 2 component
+    // - When function return value then context will be deleted which is created by th function
+
+// Callback 
